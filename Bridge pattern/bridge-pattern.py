12a9415a561284029a41f10e4d7086e2275c9d94
santiago <santiagowilchesv@gmail.com>
2022-12-11 15:26:46 -0500

    Bridge patter finished.

---
+++ Bridge pattern/bridge-pattern.py
@@ -0,0 +1,81 @@
+# It's a structural design pattern that lets you split a large class or a set of closely related classes
+# into two separate hierarchies (abstraction and implementation) which can be developed independently of each other
+
+# Elements of bridge design pattern:
+# - Abstraction: It is the core of the bridge design pattern and it provides the reference ot the implementer.
+# - Refined abstraction: It extends the abstraction to a new level where it takes the finer details one level above and hides the finer element from the implementers.
+# - Implementer: It defines the interface for implementation classes. The interface does not need to correspond directly to the abstraction interface and can be very different.
+# - Concrete implementer: Through the concrete implementation, it implements the above implementer.
+
+from abc import ABC, abstractmethod
+
+# pseudocode
+
+class Abstraction(ABC):
+    """
+    The Abstraction defines the interface for the "control" part of the two class hierarchies.
+    It mantains a reference to an object of the Implementation hierarchy and delegates all
+    of the real work to this object
+    """
+
+    def __init__(self, implementation) -> None:
+        self.implementation = implementation
+
+    def operation(self) -> str:
+        return (f"Abstraction: Base operation with: {self.implementation.operation_implementation()}")
+
+class ExtendedAbstraction(Abstraction):
+    """
+    You can extend the Abstraction without chaning the Implementation classes
+    """
+
+    def operation(self) -> str:
+        return (f"ExtendedAbstraction: Extended operation with: {self.implementation.operation_implementation()}")
+
+
+class Implementation(ABC):
+    """
+    The Implementation defines the interface for all implementation classes. It
+    doesn't have to match the Abstraction's interface. In fact, the two
+    interfaces can be entirely different. Typically the Implementation interface
+    provides only primitive operations, while the Abstraction defines higher-
+    level operations based on those primitives.
+    """
+
+    @abstractmethod
+    def operation_implementation(self) -> str:
+        pass    
+
+class ConcreteImplementationA(Implementation):
+    def operation_implementation(self) -> str:
+        return "ConcreteImplementationA: Here's the result on the platform A."
+
+class ConcreteImplementationB(Implementation):
+    def operation_implementation(self) -> str:
+        return "ConcreteImplementationB: Here's the result on the platform B."
+
+
+def client_code(abstraction: Abstraction) -> None:
+    """
+    Except for the initialization phase, where an Abstraction object gets linked
+    with a specific Implementation object, the client code should only depend on
+    the Abstraction class. This way the client code can support any abstraction-
+    implementation combination.
+    """
+    
+    print(abstraction.operation())
+
+
+if __name__ == "__main__":
+    """
+    The client code should be able to work with any pre-configured abstraction-
+    implementation combination.
+    """
+
+    implementation = ConcreteImplementationA()
+    abstraction = Abstraction(implementation)
+    client_code(abstraction)
+
+    implementation = ConcreteImplementationB()
+    abstraction = ExtendedAbstraction(implementation)
+    client_code(abstraction)
---
+++ Bridge pattern/trading-bot-example.py
@@ -0,0 +1,71 @@
+from abc import ABC, abstractmethod
+
+class Exchange(ABC): # abstraction
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def get_market_data(self, coin:str) -> list[float]:
+        pass
+
+class Binance(Exchange): # refined abstraction
+    def connect(self):
+        print(f"Connecting to Binance exchange...")
+
+    def get_market_data(self, coin: str) -> list[float]:
+        return [10, 12, 18, 14]
+
+class Coinbase(Exchange): # refined abstraction
+    def connect(self):
+        print(f"Connecting to Coinbase exchange...")
+
+    def get_market_data(self, coin: str) -> list[float]:
+        return [10, 12, 18, 20]
+
+
+class TradingBot(ABC): # implementer
+    def _init__(self, exchange: Exchange):
+        self.exchange = exchange
+
+    def check_prices(self, coin:str):
+        self.exchange.connect()
+        prices = self.exchange.get_market_data(coin)
+        should_buy = self.should_buy(prices)
+        should_sell = self.should_sell(prices)
+
+        if should_buy:
+            print(f"You should buy {coin}!")
+        elif should_sell:
+            print(f"You should sell {coin}!")
+        else:
+            print(f"No action needed for {coin}.")
+
+    @abstractmethod
+    def should_buy(self, prices: list[float]) -> bool:
+        pass
+
+    @abstractmethod
+    def should_sell(self, pirces: list[float]) -> bool:
+        pass
+
+class AverageTrader(TradingBot): # concrete implementer
+    def list_average(self, l: list[float]) -> float:
+        return sum(l) / len(l)
+
+    def should_buy(self, prices: list[float]) -> bool:
+        return prices[-1] < self.list_average(prices)
+
+    def should_sell(self, prices: list[float]) -> bool:
+        return prices[-1] > self.list_average(prices)
+
+class MinMaxTrader(TradingBot): # concrete implementer
+
+    def should_buy(self, prices: list[float]) -> bool:
+        return prices[-1] == min(prices)
+
+    def should_sell(self, prices: list[float]) -> bool:
+        return prices[-1] == max(prices) 
+
+application = AverageTrader(Coinbase())
+application.check_prices("BTC/USD")
